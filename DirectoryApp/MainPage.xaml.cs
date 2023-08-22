baa7e6343f2c2c9388ffaf35b9985ad4103ac22f
cpe3b2a06 <cpe3b2a06@CPE-PC7>
2023-08-22 16:20:28 +0800

    Add project files.

---
+++ DirectoryApp/MainPage.xaml.cs
@@ -0,0 +1,71 @@
+﻿namespace DirectoryApp
+{
+    public partial class MainPage : ContentPage
+    {
+        string setusername = "Allen Kalbo";
+        string setpassword = "PaHackSiAllen123";
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnLoginClick(object sender, EventArgs e)
+        {
+            string enteredUsername = username.Text;
+            string enteredPassword = password.Text;
+
+            if (enteredUsername == setusername)
+            {
+                if(enteredPassword == setpassword && !string.IsNullOrWhiteSpace(enteredPassword))
+                {
+                    Status.Text = "Login Successful";
+                }
+                else if(enteredPassword != setpassword && !string.IsNullOrWhiteSpace(enteredPassword))
+                {
+                    Status.Text = "Username and/or Password is incorrect. Please try again";
+                }
+                else if(string.IsNullOrWhiteSpace(enteredPassword))
+                {
+                    Status.Text = "Username and/or Password should not be empty. Please try again.";
+                }
+            }
+            else if(enteredUsername != setusername)
+            {
+                if (enteredPassword != setpassword && !string.IsNullOrWhiteSpace(enteredPassword))
+                {
+                    Status.Text = "Username and/or Password is incorrect. Please try again";
+                }
+                else if (enteredPassword == setpassword && !string.IsNullOrWhiteSpace(enteredPassword))
+                {
+                    Status.Text = "Username and/or Password is incorrect. Please try again";
+                }
+                else if(string.IsNullOrWhiteSpace(enteredPassword))
+                {
+                    Status.Text = "Username and/or Password should not be empty. Please try again.";
+                }
+            }
+            else if(enteredUsername == String.Empty)
+            {
+                if(enteredPassword == String.Empty)
+                {
+                    Status.Text = "Username and/or Password should not be empty. Please try again.";
+                }
+                if(enteredPassword != String.Empty)
+                {
+                    Status.Text = "Username and/or Password should not be empty. Please try again.";
+                }
+            }
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+        {
+            string oldText = e.OldTextValue;
+            string newText = e.NewTextValue;
+            string myText = username.Text;
+        }
+
+
+    }
+}
