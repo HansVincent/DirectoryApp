4d9947c0cacabdf0fdaa743cd4f01559eae755a5
HansVincent <vinvenjamin.gravinez.21@usjr.edu.ph>
2023-09-12 00:56:51 +0800

    Finished Product

--- DirectoryApp/Register.xaml.cs
+++ DirectoryApp/Register.xaml.cs
@@ -8,14 +8,12 @@ namespace DirectoryApp;
 
 public partial class Register : ContentPage,INotifyPropertyChanged
 {
+    StudentViewModel thisStudent = new StudentViewModel();
     public DateTime MaxDate { get; set; } = DateTime.Today;
     public DateTime MinimumDate { get; set; } = DateTime.Today.AddYears(-100);
     private ObservableCollection<string> _studentSchoolProgram;
     private ObservableCollection<string> _studentSchoolCourse;
     private ObservableCollection<string> _studentYearLevel;
-    private bool pickerSchoolProgram = false;
-    private bool pickerSchoolCourse = false;
-    private bool pickerYearLevel = false;
     public ObservableCollection<string> StudentSchoolProgram
     {
         get
@@ -57,6 +55,7 @@ public partial class Register : ContentPage,INotifyPropertyChanged
         }
 
     }
+
     public Register()
     {
         InitializeComponent();
@@ -95,66 +94,189 @@ public partial class Register : ContentPage,INotifyPropertyChanged
     private void OnDateSelected(object sender, DateChangedEventArgs e)
     {
         DateTime selectedDate = e.NewDate;
+    }
 
-        if (selectedDate == DateTime.Today)
+    private void pickerSchoolCourse_SelectedindexChanged(object sender, EventArgs e)
+    {
+        var picker = (Picker)sender;
+        int selectedIndex = picker.SelectedIndex;
+    }
+
+    private void pickerSchoolYearLevel_SelectedindexChanged(object sender, EventArgs e)
+    {
+        var picker = (Picker)sender;
+        int selectedIndex = picker.SelectedIndex;
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+
+        pickerStudentSchoolProgram.SelectedIndex = 0;
+        pickerStudentYearLevel.SelectedIndex = 0;
+        pickerStudentCourse.SelectedIndex = 0;
+        txtUserStudentIdentification.Text = String.Empty;
+        txtStudentCity.Text = String.Empty;
+        txtStudentFirstName.Text = String.Empty;
+        txtStudentLastName.Text = String.Empty;
+        txtStudentMobileNumber.Text = String.Empty;
+        txtStudentPassword.Text = String.Empty;
+        txtStudentConfirmPassword.Text = String.Empty;
+        txtStudentEmail.Text = String.Empty;
+    }
+
+    private int ValidateForm()
+    {
+        if(validateBehaviorUserStudentID.IsValid)
         {
-            borderBirthDatePicker.Stroke = Colors.Red;
+            if(validateBehaviorStudentFirstName.IsValid)
+            {
+                if(validateBehaviorStudentLastName.IsValid)
+                {
+                    if (validateBehaviorStudentPassword.IsValid)
+                    {
+                        if (validateBehaviorStudentConfirmPassword.IsValid)
+                        {
+                            if(txtStudentPassword.Text == txtStudentConfirmPassword.Text)
+                            {
+                                if (validateBehaviorStudentMobileNumber.IsValid)
+                                {
+                                    if (validateBehaviorStudentEmail.IsValid)
+                                    {
+                                        if (pickerStudentSchoolProgram.SelectedIndex != 0)
+                                        {
+                                            if (pickerStudentCourse.SelectedIndex != 0)
+                                            {
+                                                if (pickerStudentYearLevel.SelectedIndex != 0)
+                                                {
+                                                    if (rdoStudentGenderFemale.IsChecked || rdoStudentGenderMale.IsChecked)
+                                                    {
+                                                        if(studentBirthDate.Date != DateTime.Today) 
+                                                        {
+                                                            return 0;
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return 1;
+    }
+
+    private void ResetForm()
+    {
+        txtUserStudentIdentification.Text = "";
+        txtStudentFirstName.Text = "";
+        txtStudentLastName.Text = "";
+        txtStudentPassword.Text = "";
+        txtStudentConfirmPassword.Text = "";
+        txtStudentEmail.Text = "";
+        txtStudentCity.Text = "";
+        txtStudentMobileNumber.Text = "";
+        rdoStudentGenderMale.IsChecked = false;
+        rdoStudentGenderFemale.IsChecked = false;
+        pickerStudentSchoolProgram.SelectedIndex = 0;
+        pickerStudentCourse.SelectedIndex = 0;
+        pickerStudentYearLevel.SelectedIndex = 0;
+        studentBirthDate.Date = DateTime.Today;
+    }
+
+    private void ResetButtonClicked(object sender, EventArgs e)
+    {
+        ResetForm();
+    }
+
+    private async void SubmitButtonClicked(object sender, EventArgs e)
+    {
+        int valid = ValidateForm();
+        if(valid == 1)
+        {
+            await DisplayAlert("Invalid Input", "Please complete all required fields.", "OK");
+            return;
+        }
+        else if(valid == 0)
+        {
+            RegisterStudent();
+            string data = $"Student ID: {thisStudent.UserStudentID.ToString()}\n" + $"Full Name: {thisStudent.StudentLastName} {thisStudent.StudentFirstName}\n" + $"Email: {thisStudent.StudentEmail}\n" + $"Mobile No: {thisStudent.StudentMobileNumber}\n" + $"City: {thisStudent.StudentCity}\n" + $"Gender: {thisStudent.StudentGender}\n" + $"School Program: {thisStudent.StudentSchoolProgram}\n" + $"Course: {thisStudent.StudentSchoolCourse}\n" + $"Year Level: {thisStudent.StudentYearLevel}\n";
+            await DisplayAlert("Student Information", data, "OK");
         }
     }
 
+
+    private void RegisterStudent()
+    {
+        thisStudent.UserStudentID = int.Parse(txtUserStudentIdentification.Text);
+        thisStudent.StudentFirstName = txtStudentFirstName.Text;
+        thisStudent.StudentLastName = txtStudentLastName.Text;
+        thisStudent.StudentEmail = txtStudentEmail.Text;
+        thisStudent.StudentPassword = txtStudentPassword.Text;
+        thisStudent.StudentSchoolProgram = pickerStudentSchoolProgram.SelectedItem.ToString();
+        thisStudent.StudentSchoolCourse = pickerStudentCourse.SelectedItem.ToString();
+        thisStudent.StudentYearLevel = pickerStudentYearLevel.SelectedItem.ToString();
+        if(rdoStudentGenderMale.IsChecked)
+        {
+            thisStudent.StudentGender = "Male";
+        }
+        else if(rdoStudentGenderFemale.IsChecked)
+        {
+            thisStudent.StudentGender = "Female";
+        }
+        thisStudent.StudentBirthdate = studentBirthDate.Date.ToString();
+        thisStudent.StudentMobileNumber = txtStudentMobileNumber.ToString();
+        thisStudent.StudentCity = txtStudentCity.Text;
+    }
 
     private void pickerSchoolProgram_SelectedIndexChanged(object sender, EventArgs e)
     {
         var picker = (Picker)sender;
         int selectedIndex = picker.SelectedIndex;
-
-        if (selectedIndex == 0)
-        {
-            borderStudentSchoolProgram.Stroke = Colors.Red;
-        }
-        else
-        {
-            borderStudentSchoolProgram.Stroke = Colors.Black;
-        }
         StudentSchoolCourse.Clear();
-        StudentSchoolCourse.Add("-SELECT-");
+        StudentSchoolCourse.Add("-Select-");
 
         if (selectedIndex == 1)
         {
             StudentSchoolCourse.Add("Bachelor of Science in Civil Engineering");
             StudentSchoolCourse.Add("Bachelor of Science in Computer Engineering");
-            StudentSchoolCourse.Add("Bachelor of Science in Electrical Engineering");
-            StudentSchoolCourse.Add("Bachelor of Science in Electronics Engineering");
-            StudentSchoolCourse.Add("Bachelor of Science in Industrial Engineering");
             StudentSchoolCourse.Add("Bachelor of Science in Mechanical Engineering");
+            StudentSchoolCourse.Add("Bachelor of Science in Electronics Engineering");
+            StudentSchoolCourse.Add("Bachelor of Science in Electrical Engineering");
+            StudentSchoolCourse.Add("Bachelor of Science in Industrial Engineering");
         }
         else if (selectedIndex == 2)
         {
             StudentSchoolCourse.Add("Bachelor of Science in Nursing");
             StudentSchoolCourse.Add("Bachelor of Science in Medical Technology");
-       
+
         }
         else if (selectedIndex == 3)
         {
             StudentSchoolCourse.Add("Bachelor of Elementary Education");
             StudentSchoolCourse.Add("Bachelor of Early Childhood Education");
             StudentSchoolCourse.Add("Bachelor of Physical Education");
-            StudentSchoolCourse.Add("Bachelor of Secondary Education Major in English");
             StudentSchoolCourse.Add("Bachelor of Secondary Education Major in Filipino");
-            StudentSchoolCourse.Add("Bachelor of Secondary Education Major in Mathematics");
+            StudentSchoolCourse.Add("Bachelor of Secondary Education Major in English");
             StudentSchoolCourse.Add("Bachelor of Secondary Education Major in Science");
-            StudentSchoolCourse.Add("Bachelor of Special Needs Education-Generalist");
+            StudentSchoolCourse.Add("Bachelor of Secondary Education Major in Mathematics");
             StudentSchoolCourse.Add("Bachelor of Special Needs Education major in Early Childhood Education");
             StudentSchoolCourse.Add("Bachelor of Special Needs Education major in Elementary School Teaching");
+            StudentSchoolCourse.Add("Bachelor of Special Needs Education-Generalist");
         }
         else if (selectedIndex == 4)
         {
             StudentSchoolCourse.Add("Bachelor of Arts in Communication");
             StudentSchoolCourse.Add("Bachelor of Arts in Marketing Communication");
-            StudentSchoolCourse.Add("Bachelor of Arts in Journalism");
             StudentSchoolCourse.Add("Bachelor of Arts in English Language Studies");
-            StudentSchoolCourse.Add("Bachelor of Science in Biology major in Medical Biology");
+            StudentSchoolCourse.Add("Bachelor of Arts in Journalism");
             StudentSchoolCourse.Add("Bachelor of Science in Biology major in Microbiology");
+            StudentSchoolCourse.Add("Bachelor of Science in Biology major in Medical Biology");
             StudentSchoolCourse.Add("Bachelor of Science in Psychology");
             StudentSchoolCourse.Add("Bachelor of Library and Information Science");
             StudentSchoolCourse.Add("Bachelor of Arts in International Studies");
@@ -188,6 +310,8 @@ public partial class Register : ContentPage,INotifyPropertyChanged
         }
 
     }
+
+
 
 
 }
